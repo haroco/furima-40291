8ea8524438f42c42cd6097ac749ed1a54c9448ff
haroco <batiankuanzi@gmail.com>
2024-04-20 20:29:39 +0900

    マイグレーションファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2024_04_18_102108) do
   create_table "users", charset: "utf8", force: :cascade do |t|
-    t.string "nickame", null: false
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "family_name", null: false
