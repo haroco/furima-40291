107234252436ea7335f229e05529251ffb82a1af
haroco <batiankuanzi@gmail.com>
2024-04-20 22:49:09 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,21 +6,11 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :birth_date, presence: true
-
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Password is invalid. Include both letters and numbers' 
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Last name is invalid. Input full-width characters' } do
-    validates :family_name
-  end
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'First name is invalid. Input full-width characters' } do
-    validates :first_name
-  end
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Last name kana is invalid. Input full-width katakana characters' } do
-    validates :family_name_kana
-  end
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'First name kana is invalid. Input full-width katakana characters' } do
-    validates :first_name_kana
-  end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers
+  ' }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ , message: "Input full-width characters" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ , message: "Input full-width characters" }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ , message: "Input full-width katakana characters" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ , message: "Input full-width katakana characters" }
   
 end
