16167e4f17e0fa30517beb83f0b456ee5f40bb00
haroco <batiankuanzi@gmail.com>
2024-04-27 20:36:51 +0900

    ActiveHashで商品の状態を作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,9 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :Condition
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
 
 end
