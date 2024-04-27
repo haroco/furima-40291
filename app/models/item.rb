708f008d5a9c278131e0089ccb68a5d2ea7dde32
haroco <batiankuanzi@gmail.com>
2024-04-27 21:31:05 +0900

    ActiveHashで"発送までの日数"を作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,10 +6,12 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :shipping_fee
   belongs_to :prefecture
+  belongs_to :delivery_time
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"} 
 
 end
