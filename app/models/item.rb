df91906713e8511d8ca91a310f5f778b885d113e
haroco <batiankuanzi@gmail.com>
2024-04-27 21:21:02 +0900

    ActiveHashで"発送元の地域"を作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,12 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
-  belongs_to :Shipping_fee
+  belongs_to :shipping_fee
+  belongs_to :prefecture
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :Shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
 
 end
