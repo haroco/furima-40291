7f1f27718afdec5123b232eee42a264d019d3535
haroco <batiankuanzi@gmail.com>
2024-04-27 20:53:33 +0900

    ActiveHashで"配送料の負担"を作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,11 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  belongs_to :Condition
+  belongs_to :condition
+  belongs_to :Shipping_fee
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :Shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"} 
 
 end
