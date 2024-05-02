b4e5f7920f38a8af8c87ebcba44720df2ff55bb0
haroco <batiankuanzi@gmail.com>
2024-05-02 19:04:23 +0900

    半角整数値のみ許容するバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,5 +17,6 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_time_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ },
+  numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
