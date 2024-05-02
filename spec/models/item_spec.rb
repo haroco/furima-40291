a7168bb14346167a1fe06abb2d5c5d4e0933fd2e
haroco <batiankuanzi@gmail.com>
2024-05-02 19:43:20 +0900

    userが紐づいていないと保存できないことを確認するテストコードを追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -95,7 +95,12 @@ RSpec.describe Item, type: :model do
         @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
-      end      
+      end 
+      it 'userが紐づいていなければ保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end     
     end
   end
 end
