9bac691415a51274373b5f890520668959714e16
haroco <batiankuanzi@gmail.com>
2024-05-19 19:32:11 +0900

    情報が保存できるように実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,25 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])  
   end  
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:item_id, :post_code, :prefecture_id, :city, :street_address, :building_name, :tel_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
